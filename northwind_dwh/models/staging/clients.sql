7d292b53e5346729b929b536e669478007011fe6
FredericPerret <perretfrederic.simplon@gmail.com>
2026-08-04 11:25:42 +0200

    ajout projet DBT northwind_dwh

---
+++ northwind_dwh/models/staging/clients.sql
@@ -0,0 +1,12 @@
+
+{{ config(materialized='table') }}
+
+SELECT CAST(UPPER(customer_id) AS VARCHAR(5)) AS customer_id, 
+	   CAST(TRIM(REGEXP_REPLACE(company_name,'\s+',' ','g')) AS VARCHAR(40)) AS company_name, 
+	   CAST(INITCAP(TRIM(REGEXP_REPLACE(contact_name,'\s+',' ','g'))) AS VARCHAR(30)) AS contact_name, 
+	   CAST(INITCAP(TRIM(REGEXP_REPLACE(contact_title,'\s+',' ','g'))) AS VARCHAR(30)) AS contact_title, 
+	   CAST(INITCAP(TRIM(REGEXP_REPLACE(city,'\s+',' ','g'))) AS VARCHAR(15)) AS city, 
+	   CAST(INITCAP(TRIM(REGEXP_REPLACE(country,'\s+',' ','g'))) AS VARCHAR(15)) AS country, 
+	   CAST(TRIM(phone) AS VARCHAR(24)) AS phone
+FROM {{ source('northwind', 'customers') }}
+
---
+++ northwind_dwh/models/staging/commandes.sql
@@ -0,0 +1,17 @@
+
+{{ config(materialized='table') }}
+
+SELECT order_id, 
+	   CAST(UPPER(customer_id) AS VARCHAR(5)) AS customer_id, 
+	   employee_id, 
+	   order_date, 
+	   required_date, 
+	   shipped_date,
+	   CASE WHEN shipped_date IS NULL THEN False ELSE True END AS is_shipped,
+	   ship_via,
+	   freight,
+	   CAST(INITCAP(TRIM(REGEXP_REPLACE(ship_name,'\s+',' ','g'))) AS VARCHAR(40)) AS ship_name,
+	   CAST(INITCAP(TRIM(REGEXP_REPLACE(ship_city,'\s+',' ','g'))) AS VARCHAR(15)) AS ship_city,
+	   CAST(INITCAP(TRIM(REGEXP_REPLACE(ship_country,'\s+',' ','g'))) AS VARCHAR(15)) AS ship_country
+FROM {{ source('northwind', 'orders') }}
+
