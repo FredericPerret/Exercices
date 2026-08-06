2db11f7f9bba59273657979cad9897a6964deaa1
FredericPerret <perretfrederic.simplon@gmail.com>
2026-08-06 11:34:00 +0200

    Q19

--- northwind_dwh/models/staging/stg_orders.sql
+++ northwind_dwh/models/staging/stg_orders.sql
@@ -8,7 +8,6 @@ SELECT order_id,
 	   CASE WHEN shipped_date IS NULL THEN False ELSE True END AS is_shipped,
 	   ship_via,
 	   freight,
-	   CAST(INITCAP(TRIM(REGEXP_REPLACE(ship_name,'\s+',' ','g'))) AS VARCHAR(40)) AS ship_name,
 	   CAST(INITCAP(TRIM(REGEXP_REPLACE(ship_city,'\s+',' ','g'))) AS VARCHAR(15)) AS ship_city,
 	   CAST(INITCAP(TRIM(REGEXP_REPLACE(ship_country,'\s+',' ','g'))) AS VARCHAR(15)) AS ship_country
 FROM {{ source('northwind', 'orders') }}
